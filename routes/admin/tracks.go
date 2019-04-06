f0af68a2863765d78f05849f5167db492aab516c
Aurèle Oulès <aurele@oules.com>
2019-04-06 19:02:50 +0200

    typo

--- routes/admin/tracks.go
+++ routes/admin/tracks.go
@@ -7,7 +7,7 @@ import (
 
 func handleTracks(r *mux.Router) {
 	r.Handle("/tracks/{name}/indexes", ProtectedRoute(adminHandlers.UpdateTracksIndexes)).Methods("PUT")
-	r.Handle("/tracks/{name}/{groupid}", ProtectedRoute(adminHandlers.AddTrack)).Methods("POST")
+	r.Handle("/tracks/{name}/{albumid}", ProtectedRoute(adminHandlers.AddTrack)).Methods("POST")
 
 	r.Handle("/tracks/{name}/{id}", ProtectedRoute(adminHandlers.GetTrack)).Methods("GET")
 	r.Handle("/tracks/{name}/{id}", ProtectedRoute(adminHandlers.DeleteTrack)).Methods("DELETE")
